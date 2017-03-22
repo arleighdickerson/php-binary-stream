2215e96345c22907ff7487aade05483e0831db21
Arleigh Dickerson (arls) <arls@arleighdickerson.us>
2017-03-22 17:19:31 -0400

    Update BinaryClient.php

--- src/php/BinaryClient.php
+++ src/php/BinaryClient.php
@@ -4,11 +4,11 @@ namespace arls\binarystream;
 
 class Client {
     use BinaryHandlers;
-	
-	private $_url;
-	
-	public function __construct($url){
-		$this->_url = $url;
+
+    private $_url;
+
+    public function __construct($url) {
+        $this->_url = $url;
     }
 
     public function run() {
@@ -18,18 +18,18 @@ class Client {
                 /** @var BinaryNode $node */
                 $unpacked = $this->unpack($data);
                 list($type, $payload, $bonus) = $unpacked;
-				$this->invokeHandler($type, $payload, $bonus);
-			});
-			$this->onOpen($ws);
+                $this->invokeHandler($type, $payload, $bonus);
+            });
+            $this->onOpen($ws);
         }, function ($e) {
             echo "Could not connect: {$e->getMessage()}\n";
         });
-	}
+    }
 
-	public function onOpen(Ratchet\Client\WebSocket $ws){
-		//override THIS one
-		return;
-	}
+    public function onOpen(Ratchet\Client\WebSocket $ws) {
+        //override THIS one
+        return;
+    }
 
     public function getClient() {
     }
@@ -50,4 +50,14 @@ class Client {
         return self::$_unpacker->unpack($value);
     }
 }
+     */
+    private static $_unpacker;
 
+    private static function unpack($value) {
+        if (self::$_unpacker === null) {
+            $bufferUnpacker = new BufferUnpacker();
+            self::$_unpacker = new Unpacker($bufferUnpacker);
+        }
+        return self::$_unpacker->unpack($value);
+    }
+}
